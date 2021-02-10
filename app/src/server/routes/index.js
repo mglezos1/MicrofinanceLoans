ed5d97639e78748cc826f7e2b3e08cef4c8ce650
Felipe Muriel <murielfelipe@hotmail.com>
2021-02-10 05:03:44 -0500

    Loan service and oracle rates

--- app/src/server/routes/index.js
+++ app/src/server/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const axios = require('axios');
 
 var connectionDB = require("./database");
 
@@ -9,6 +10,24 @@ router.get("/", function (req, res, next) {
 });
 
 /* GET home page. */
+router.get('/rates',  async function(req, res, next) {
+  axios.get('https://www.quandl.com/api/v3/datasets/FRED/DPRIME.json?api_key=sbVoyriisY6fszmUcehV')
+  .then(response => {
+    let stock = {};
+    let dataNowDayRates =  response.data.dataset.data[0];
+    stock.rate=dataNowDayRates[1];
+    stock.date=dataNowDayRates[0];
+    console.log('Rate',stock.rate)
+    console.log('Date',stock.date)
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(stock,1,4));
+  })
+  .catch(error => {
+    console.log(error);
+  });  
+});
+
+/* GET home page. 
 router.post("/save", function (req, res, next) {
   console.log()
   let { body } = req;
@@ -22,6 +41,6 @@ router.post("/save", function (req, res, next) {
     console.log(`${body.valName} auto-generated ID:, ${jane.id}`);
     res.send({body});
   });
-});
+});*/
 
 module.exports = router;
