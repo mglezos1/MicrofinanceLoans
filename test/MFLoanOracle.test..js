6311aeef7130ae784f1bf120765b571e027a1c81
mglezos <mglezos1@gmail.com>
2021-02-12 00:21:06 -0500

    uploading Oracle test file

--- test/MFLoanOracle.test..js
+++ test/MFLoanOracle.test..js
@@ -1,15 +1,25 @@
-//not done just wanted to push something before going to bed
-
 const BigNumber = require("bignumber.js");
 const truffleAssert = require("truffle-assertions");
-const MFOracle = artifacts.require("MFLoadOracle");
+const MFLoanOracle = artifacts.require("MFLoan");
 
-contract("interestRate", (accounts)=>{
+contract("MFLoanOracle", function (accounts){
+    describe("Oracle Deployment", () =>{
     it('should deploy', async () => {
-        const interestInstance = await interestRate.deployed();
+        const instance = await MFLoanOracle.deployed();
         assert.isTrue(instance ? true : false)
-      });
-    it('should get the interest rate', async()=>{
-        const insertInstance = await interestRate.deployed();
+    });
+});
+    describe("Interest Rate", async()=>{
+        it('should get the interest rate', async()=>{
+            const instance = await MFLoanOracle.deployed();
+            const rate = await instance.getRate();
+            assert(rate<0);
+        })
+
+        it('should not be less than 1', async()=>{
+            const instance = await MFLoanOracle.deployed();
+            const rate100 = await instance.setRate();
+            assert(rate100<1);
+        })
     })
-})
+});
